65247185ac5428cbdd35002b33e3fc895a1721ec
Romario Work <romario.g@estrela10.com.br>
2023-06-24 12:03:37 -0300

    Feat(Update typeorm config): add

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,9 +1,20 @@
-import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import {
+  Connection,
+  createConnection,
+  getConnection,
+  getConnectionOptions,
+} from "typeorm";
 
 export default async (
   host = "containers-us-west-76.railway.app",
 ): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  const connection = getConnection();
+  if (connection.isConnected) {
+    console.log("Conectado com sucesso!");
+  } else {
+    console.log("Falha ao conectar ao banco de dados.");
+  }
 
   return createConnection(
     Object.assign(defaultOptions, {

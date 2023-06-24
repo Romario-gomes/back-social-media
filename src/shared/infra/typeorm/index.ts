88cc7efabfdac0edc47643566fbadf4f79938227
Romario Work <romario.g@estrela10.com.br>
2023-06-24 11:23:42 -0300

    Update Configs deploy: update package seed

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,8 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "localhost"): Promise<Connection> => {
+export default async (
+  host = "containers-us-west-76.railway.app",
+): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(

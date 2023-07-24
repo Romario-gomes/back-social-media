c62c355f6fffa830e3c6a2fa39d9eb44e2e69c87
Romario Work <romario.g@estrela10.com.br>
2023-07-24 20:43:03 -0300

    Feat node version

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,8 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "dpg-ciu1gvh5rnuhcnsg1rog-a"): Promise<Connection> => {
+export default async (
+  host = "dpg-ciu1gvh5rnuhcnsg1rog-a",
+): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(

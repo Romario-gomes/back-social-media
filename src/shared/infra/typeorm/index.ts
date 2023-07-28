3332aeef94bb45785fbfe45ddf62f307db149b3b
Romario Work <romario.g@estrela10.com.br>
2023-07-28 18:22:56 -0300

    test

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,13 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (): Promise<Connection> => {
+export default async (
+  host = "dpg-cj21sbh5rnuukffomsc0-a",
+): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
+      host,
       database: defaultOptions.database,
     }),
   );

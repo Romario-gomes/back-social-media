ef36cd5d79553baca89bf9c52f5ee0447c4f413c
Romario Work <romario.g@estrela10.com.br>
2023-07-29 15:12:05 -0300

    Feat(Vercel test): add

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,7 +1,7 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (
-  host = "dpg-cj21sbh5rnuukffomsc0-a",
+  host = "dpg-cj21sbh5rnuukffomsc0-a.oregon-postgres.render.com",
 ): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 

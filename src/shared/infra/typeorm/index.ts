31897fe30cfdca19e660fcc87ebdfc577967dc13
Romario Work <romario.g@estrela10.com.br>
2023-08-01 11:00:06 -0300

    Feat(Railway configs): add

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,7 +2,6 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  console.log("Conections: ", defaultOptions);
 
   return createConnection(
     Object.assign(defaultOptions, {
